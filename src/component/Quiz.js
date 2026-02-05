e37e0ca8317b908b2d35b7ae50d5b005010c28b4
Peerapong9947 <s6703052411414@email.kmutnb.ac.th>
2026-02-05 10:36:08 +0700

    Version1

---
+++ src/component/Quiz.js
@@ -0,0 +1,11 @@
+import QuestionsData from "../data/QuestionsData";
+
+const Quiz = ()=>{
+    console.log(QuestionsData);
+    return(
+        <div className="quiz">
+            <h1>Quiz Component</h1>
+        </div>
+    )
+}
+export default Quiz;
